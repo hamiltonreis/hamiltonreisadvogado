20b21e31da993da3eaba67b2b997091f654554b8
Hamilton Reis <hamiltonreisparreiras@gmail.com>
2026-02-09 10:32:45 -0300

    Atualização Icone Facebook

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -5,7 +5,7 @@ import './SocialIcons.css'
 const SocialIcons = (props) => {
 	return (
 		<div className={props.class}>
-			<a target="_blank" title="Nossa página no Facebook" href="https://facebook.com" rel="noreferrer"><i className="fa fa-facebook"></i></a>
+			<a target="_blank" title="Nossa página no Facebook" href="https://www.facebook.com/people/Hamilton-Reis-Advocacia/61587226859776/" rel="noreferrer"><i className="fa fa-facebook"></i></a>
 			<a target="_blank" title="Nosso perfil no Instagram" href="https://instagram.com/hamiltonreis.adv" rel="noreferrer"><i className="fa fa-instagram"></i></a>
 		</div>
 	);
